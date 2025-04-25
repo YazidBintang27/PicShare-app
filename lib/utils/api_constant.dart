45ab403daf401404d1699e294b51727da187b698
Yazid Bintang <yazidbintang2711@gmail.com>
2025-04-25 23:57:21 +0700

    Add API endpoint

---
+++ lib/utils/api_constant.dart
@@ -0,0 +1,6 @@
+class ApiConstant {
+  static const baseUrl = 'https://story-api.dicoding.dev/v1';
+  static const register = '/register';
+  static const login = '/login';
+  static const stories = '/stories';
+}
