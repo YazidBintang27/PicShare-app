383849d7251470515516296255e3fb494c3b5579
Yazid Bintang <yazidbintang2711@gmail.com>
2025-06-03 10:59:14 +0700

    Add code generation for request model

--- lib/data/remote/models/request/register_request.dart
+++ lib/data/remote/models/request/register_request.dart
@@ -1,12 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'register_request.g.dart';
+
+@JsonSerializable()
 class RegisterRequest {
   final String name;
   final String email;
   final String password;
 
-  RegisterRequest(
-      {required this.name, required this.email, required this.password});
+  RegisterRequest({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
 
-  Map<String, dynamic> toJson() {
-    return {'name': name, 'email': email, 'password': password};
-  }
+  // fromJson factory
+  factory RegisterRequest.fromJson(Map<String, dynamic> json) =>
+      _$RegisterRequestFromJson(json);
+
+  // toJson method
+  Map<String, dynamic> toJson() => _$RegisterRequestToJson(this);
 }
